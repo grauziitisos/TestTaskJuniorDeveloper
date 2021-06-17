15bb18718eabdab57a026d61305bde9523a53792
Oskars Grauzis <oskars_grauzis@inbox.lv>
2021-06-17 22:19:05 +0300

    for hosting
    hosting fixes

--- Task3/lib/Database.php
+++ Task3/lib/Database.php
@@ -9,6 +9,8 @@ class Database extends PDO
      */
     public function __construct()
     {
-        parent::__construct("mysql:host=localhost;dbname=subscriptions", "subscriptions", "subscriptions");
+        //parent::__construct("mysql:host=localhost;dbname=subscriptions", "subscriptions", "subscriptions");
+        //parent::__construct("mysql:host=sql104.epizy.com;dbname=epiz_28906882_subscriptions", "epiz_28906882", "ugnsy5ad2pRUFRP");
+        parent::__construct("mysql:host=fdb31.runhosting.com;dbname=3874475_subscriptions", "3874475_subscriptions", "subscriptions123");
     }
 }
--- Task3/views/header.php
+++ Task3/views/header.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="public/default.css">
+    <link rel="stylesheet" href="public/Default.css">
     <script
             src="https://code.jquery.com/jquery-3.6.0.min.js"
             integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
